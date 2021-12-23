01bdd102a77e54ab3c840bbb2d319ac67684f250
Mauro Cioni <mcioni@imolainformatica.it>
2021-12-23 07:28:19 +0100

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'cypress-debugger';
+
+  ngOnInit() {
+    let a = 0;
+    console.log(a++);
+    console.log(a++);
+    console.log(a++);
+    console.log(a++);
+    console.log(a++);
+    console.log(a++);
+    console.log(a++);
+    console.log(a++);
+    console.log(a++);
+    console.log(a++);
+    console.log(a++);
+    console.log(a++);
+    console.log(a++);
+    console.log(a++);
+    console.log(a++);
+  }
 }
